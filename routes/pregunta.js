99c92b1b97e72d18b4c646dd074fa25003568693
RoyElProgrammer <a01424454@tec.mx>
2022-05-06 09:56:57 -0500

    Obtener preguntas

--- routes/pregunta.js
+++ routes/pregunta.js
@@ -4,7 +4,7 @@ const preguntaController = require('../controller/pregunta.controller');
 
 const middleware = require('../middleware/jwt-middleware.js');
 
-router.get('/pregunta', middleware, preguntaController.getPreguntas);
+router.get('/preguntas', middleware, preguntaController.getPreguntas);
 router.get('/pregunta/:id', middleware, preguntaController.getPregunta);
 router.put('/pregunta', middleware, preguntaController.updatePregunta);
 router.post('/pregunta', middleware, preguntaController.insertPregunta);
