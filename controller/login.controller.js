ba8b2bdedf48b465452884fcbb48f1b48fb2484a
RoyElProgrammer <a01424454@tec.mx>
2022-05-05 23:03:27 -0500

    Repetidos cheeeck

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -63,18 +63,45 @@ module.exports.insertUsuario = (req, res) =>
     start = dataValidation.intCheck(body.skin,start);
     
     if(start){
-
-        const sql = `INSERT INTO usuario(idGremio, userName, correo, contrasenia, 
-            rol, edad, skin, nivel, experiencia, puntos, horasJuego, wins, loses, 
-            vida, mana, dano, defensa) VALUES (6,?,?,SHA2(?,224),?,?,?,0,0,0,0,0,
-            0,100,100,20,10)`;
-        conexion.query(sql, [body.userName, body.correo,
-            body.contrasenia, body.rol, body.edad, body.skin], 
-            (error, results, fields) =>{
-            if(error){
-                res.json("Error al crear el usuario");
+        const sql1 = `SELECT idUsuario FROM usuario WHERE userName = ?`;
+        conexion.query(sql1, [body.userName], (error1, results1, fields) =>{
+            if(error1){
+                res.json("Error en la conexión");
             }
-            res.json("Usuario creado");
+            let result = Object.values(JSON.parse(JSON.stringify(results1)));
+            let arrtemp = result.map(object => object.idUsuario);
+            let idUser = arrtemp[0];
+            if(!(!isNaN(idUser) && idUser > 0)){
+                const sql2 = `SELECT idUsuario FROM usuario WHERE correo = ?`;
+                conexion.query(sql2, [body.correo], (error2, results2, fields) =>{
+                    if(error2){
+                        res.json("Error en la conexión");
+                    }
+                    let result1 = Object.values(JSON.parse(JSON.stringify(results2)));
+                    let arrtemp1 = result1.map(object => object.idUsuario);
+                    let idUser1 = arrtemp1[0];
+                    if(!(!isNaN(idUser1) && idUser1 > 0)){
+                        const sql = `INSERT INTO usuario(idGremio, userName, correo, contrasenia, 
+                            rol, edad, skin, nivel, experiencia, puntos, horasJuego, wins, loses, 
+                            vida, mana, dano, defensa) VALUES (6,?,?,SHA2(?,224),?,?,?,0,0,0,0,0,
+                            0,100,100,20,10)`;
+                        conexion.query(sql, [body.userName, body.correo,
+                            body.contrasenia, body.rol, body.edad, body.skin], 
+                            (error, results, fields) =>{
+                            if(error){
+                                res.json("Error al crear el usuario");
+                            }
+                            res.json("Usuario creado");
+                        });
+                    }
+                    else{
+                        res.json("Correo existente")
+                    }
+                });
+            }
+            else{
+                res.json("Usuario existente")
+            }
         });
     }
     else{
